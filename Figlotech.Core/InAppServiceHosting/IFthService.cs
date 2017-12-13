de98bacb2386ec76d8ff7bbdf518e9cb8838efe9
Felype Rennan <felyperennan@hotmail.com>
2017-12-13 12:35:45 -0200

    Adds basic in-app 'service' control.

---
+++ Figlotech.Core/InAppServiceHosting/IFthService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Figlotech.Core.InAppServiceHosting
+{
+    public class FthServiceStatus {
+        public bool IsRunning { get; set; }
+        public bool CanStart { get; set; }
+        public string StatusText { get; set; }
+    }
+    public interface IFthService
+    {
+        Thread Thread { get; set; }
+        Dictionary<string, object> Configuration { get; }
+        FthServiceStatus GetStatus();
+        void Init(params string[] args);
+
+        string Exec(string command);
+
+        void Run();
+    }
+}
---
+++ Figlotech.Core/InAppServiceHosting/ServiceHost.cs
@@ -0,0 +1,32 @@
+﻿using Figlotech.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Figlotech.Core.InAppServiceHosting {
+    public class ServiceHost
+    {
+        List<IFthService> Services = new List<IFthService>();
+        public static ServiceHost Default = new ServiceHost();
+
+
+        public IFthService Run<T>(params string[] args) where T : IFthService, new() {
+            return Run(typeof(T), args);
+        }
+        public IFthService Run(Type serviceType, params string[] args) {
+            if(!typeof(IFthService).IsAssignableFrom(serviceType)) {
+                throw new Exception($"Specified service class {serviceType.Name} is not an implementation of IFthService interface.");
+            }
+            IFthService service = (IFthService) Activator.CreateInstance(serviceType);
+            service.Init(args);
+            service.Thread = new Thread(service.Run);
+            Services.Add(service);
+
+            service.Thread.Start();
+            return service;
+        }
+    }
+}
