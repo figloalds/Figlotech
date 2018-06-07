c38417439e3c170dc0ff065afd7537a75f9acb68
Felype Rennan <felyperennan@hotmail.com>
2018-06-07 13:48:33 -0300

    Minor improvement to HourlySyncCode.

--- Figlotech.Core/HourlySyncCode.cs
+++ Figlotech.Core/HourlySyncCode.cs
@@ -1,32 +1,53 @@
 ﻿using System;
+using System.Linq;
 
 namespace Figlotech.Core {
     public class HourlySyncCode {
-        public static String Generate(String Password = null) {
+        public static HourlySyncCode Generate(String Password = null) {
             return Generate(DateTime.UtcNow, Password);
         }
 
-        public static String Generate(DateTime KeyMomment, String Password = null) {
+        private HourlySyncCode(byte[] code) {
+            Code = code;
+        }
+
+        byte[] Code { get; set; }
+
+        public static implicit operator String(HourlySyncCode code) {
+            return Convert.ToBase64String(code.Code);
+        }
+        public static implicit operator HourlySyncCode(String code) {
+            return new HourlySyncCode(Convert.FromBase64String(code));
+        }
+
+        public static implicit operator byte[](HourlySyncCode code) {
+            return code.Code;
+        }
+        public static implicit operator HourlySyncCode(byte[] code) {
+            return new HourlySyncCode(code);
+        }
+
+        public static HourlySyncCode Generate(DateTime KeyMomment, String Password = null) {
             CrossRandom cs = new CrossRandom((KeyMomment.Date.AddHours(KeyMomment.Hour)).Ticks);
             byte[] barr = new byte[64];
             for(int i = 0; i < barr.Length; i++) {
                 barr[i] = (byte)cs.Next(256);
             }
-            return Convert.ToBase64String(barr);
+            return new HourlySyncCode(barr);
         }
 
-        public static bool Validate(DateTime keyMoment, String code, String Password) {
+        public static bool Validate(DateTime keyMoment, HourlySyncCode code, String Password) {
             return Validate(code, Password, keyMoment);
         }
-        public static bool Validate(String code, String Password, DateTime? keyMoment = null) {
+        public static bool Validate(HourlySyncCode code, String Password, DateTime? keyMoment = null) {
             keyMoment = keyMoment ?? DateTime.UtcNow;
-            if (Generate(Password) == code) {
+            if (Generate(Password).Code.SequenceEqual(code.Code)) {
                 return true;
             }
 
             for (int i = 3; i >= 0; --i) {
                 var dt = DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(i));
-                if (Generate(dt, Password) == code) {
+                if (Generate(dt, Password).Code.SequenceEqual(code.Code)) {
                     return true;
                 }
             }
@@ -34,7 +55,7 @@ namespace Figlotech.Core {
             for (int i = 0; i >= 2; ++i) {
                 var v = i - 1;
                 var dt = DateTime.UtcNow.AddHours(i);
-                if (Generate(dt, Password) == code) {
+                if (Generate(dt, Password).Code.SequenceEqual(code.Code)) {
                     return true;
                 }
             }
