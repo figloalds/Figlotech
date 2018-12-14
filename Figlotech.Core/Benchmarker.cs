63621b1a10e635d51a3e6f2bbd77c7132107534b
Felype Rennan <felyperennan@hotmail.com>
2018-12-14 16:33:17 -0200

    Adds more problem tracking functionality

--- Figlotech.Core/Benchmarker.cs
+++ Figlotech.Core/Benchmarker.cs
@@ -2,10 +2,15 @@
 using Figlotech.Core.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace System {
+
+
     public static class BenchMarkerExtensions {
-        public static double Mark(this Benchmarker self, String txt) {
+        public static double Mark(this Benchmarker self, String txt, Exception ex = null) {
             if (!FiTechCoreExtensions.EnableBenchMarkers)
                 return 0;
             if (self == null) return 0;
@@ -14,7 +19,9 @@ namespace System {
             try {
                 if(self.marks.Count > 1)
                     self.marks[self.marks.Count - 1].SetDuration(DateTime.UtcNow);
-                self.marks.Add(new TimeMark(self.marks[0].Timestamp, txt));
+                self.marks.Add(new TimeMark(self.marks[0].Timestamp, txt) {
+                    Exception = ex
+                });
                 if (self.marks.Count < 2)
                     return 0;
                 var retv = self.marks[self.marks.Count - 1]
@@ -26,6 +33,95 @@ namespace System {
             return 0;
         }
 
+        public static async Task<T> Wrap<T>(this Benchmarker self, string label, Func<Task<T>> action) {
+            T retv;
+            try {
+                self.Mark($"[RegionStart] {label}");
+                retv = await action.Invoke();
+                self.Mark($"[RegionEndOk] {label}");
+            }
+            catch (Exception x) {
+                self.Mark($"[RegionException] {label}", x);
+                retv = default(T);
+                throw x;
+            }
+
+            return retv;
+        }
+
+        public static T Wrap<T>(this Benchmarker self, string label, Func<T> action) {
+            T retv;
+            try {
+                self.Mark($"[RegionStart] {label}");
+                retv = action.Invoke();
+                self.Mark($"[RegionEndOk] {label}");
+            } catch(Exception x) {
+                self.Mark($"[RegionException] {label}", x);
+                retv = default(T);
+                throw x;
+            }
+
+            return retv;
+        }
+
+        public static void Wrap(this Benchmarker self, string label, Action action) {
+            try {
+                self.Mark($"[RegionStart] {label}");
+                action.Invoke();
+                self.Mark($"[RegionEndOk] {label}");
+            }
+            catch (Exception x) {
+                self.Mark($"[RegionException] {label}", x);
+                throw x;
+            }
+        }
+
+        public static void Assert(this Benchmarker self, Expression<Func<bool>> expr) {
+            if (!FiTechCoreExtensions.EnableBenchMarkers ||  expr == null)
+                return;
+            try {
+                bool result = expr.Compile().Invoke();
+                if (!result && Debugger.IsAttached) {
+                    self.Mark($"Assert Failed {expr.ToString()} => {result}");
+                    Debugger.Break();
+                }
+            } catch(Exception x) {
+                self.Mark($"Assert {expr.ToString()} => Exception", x);
+                if (Debugger.IsAttached) {
+                    Debugger.Break();
+                }
+            }
+        }
+
+        public static IEnumerable<String> VerboseLog(this Benchmarker self) {
+            if (!FiTechCoreExtensions.EnableBenchMarkers)
+                yield break;
+            if (self == null)
+                yield break;
+
+            var lines = new List<String>();
+
+            var retv = (self.marks[self.marks.Count-1].Timestamp - self.marks[0].Timestamp).TotalMilliseconds;
+            yield return ($"--------------------------");
+            yield return ($"{self.myName}");
+            yield return ($"--------------------------");
+            yield return ($" | 0ms ");
+            for (int i = 0; i < self.marks.Count - 1; i++) {
+                yield return ($" | [{i}] {self.marks[i].Name} +[{self.marks[i].Duration.TotalMilliseconds}ms]");
+                if (self.marks[i].Exception != null) {
+                    var ex = self.marks[i].Exception;
+                    do {
+                        yield return ($" | [{i}] => Thrown {self.marks[i].Exception.Message}");
+                        yield return ($" | [{i}] => StackTrace {self.marks[i].Exception.StackTrace}");
+                        ex = ex.InnerException;
+                    } while (ex != null);
+                }
+            }
+            yield return ($" v {retv}ms");
+            yield return ($"--------------------------");
+            yield return ($"Total: {retv}ms");
+        }
+
         public static double FinalMark(this Benchmarker self) {
             if (!FiTechCoreExtensions.EnableBenchMarkers)
                 return 0;
@@ -33,24 +129,15 @@ namespace System {
             if (!self.Active)
                 return 0;
             try {
+                var initMark = self.marks[0];
                 if (self.marks.Count < 2)
-                    self.marks.Add(new TimeMark(self.marks[0].Timestamp, self.myName));
-                self.marks[self.marks.Count - 1].SetDuration(DateTime.UtcNow);
-                var retv = self.marks[self.marks.Count - 1].Timestamp
-                    .Subtract(self.marks[0].Timestamp).TotalMilliseconds;
+                    self.marks.Add(new TimeMark(initMark.Timestamp, self.myName));
+                var lastMark = self.marks[self.marks.Count - 1];
+                lastMark.SetDuration(DateTime.UtcNow);
+                var retv = (self.marks[self.marks.Count - 1].Timestamp - self.marks[0].Timestamp).TotalMilliseconds;
                 if (self.WriteToStdout) {
                     lock ("BENCHMARKER BM_WRITE") {
-                        Console.WriteLine($"-- PERFORMANCE -----------");
-                        Console.WriteLine($"{self.myName}");
-                        Console.WriteLine($"--------------------------");
-                        double line = 0;
-                        Console.WriteLine($" | 0ms ");
-                        for (int i = 0; i < self.marks.Count - 1; i++) {
-                            Console.WriteLine($" | {self.marks[i].Name} +[{self.marks[i].Duration.TotalMilliseconds}ms]");
-                        }
-                        Console.WriteLine($" v {retv}ms");
-                        Console.WriteLine($"--------------------------");
-                        Console.WriteLine($"Total: {retv}ms");
+                        self.VerboseLog().ForEach(Console.WriteLine);
                     }
                 }
                 return retv;
@@ -60,9 +147,10 @@ namespace System {
     }
 
     internal class TimeMark {
-        public DateTime Timestamp;
-        public TimeSpan Duration;
-        public String Name;
+        public DateTime Timestamp { get; set; }
+        public TimeSpan Duration { get; set; }
+        public String Name { get; set; }
+        public Exception Exception { get; set; }
 
         public TimeMark(DateTime start, String txt) {
             Timestamp = DateTime.UtcNow;
