47d09a688158a2cb63d450b52c2b975ee76d32d7
Felype Rennan <felyperennan@hotmail.com>
2019-05-28 11:06:41 -0300

    Implements noop encryptor, for not encrypting data.

---
+++ Figlotech.Core/Autokryptex/EncryptionMethods/NoOpEncryptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figlotech.Core.Autokryptex.EncryptionMethods {
+    public class NoOpEncryptor : IEncryptionMethod {
+        public byte[] Decrypt(byte[] en) {
+            return en;
+        }
+
+        public byte[] Encrypt(byte[] en) {
+            return en;
+        }
+    }
+}
