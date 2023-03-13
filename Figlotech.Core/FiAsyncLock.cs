b154dc7424348e870bc0e055ffe348eddd5141eb
Felype Rennan <felyperennan@hotmail.com>
2023-03-13 19:21:04 -0300

    Significant improvement on FiAsyncLock
    A huge problem with this is that Lock is async and returns disposable, the user code may forget the await keyword and dispose the task instead of the disposable lock release

--- Figlotech.Core/FiAsyncLock.cs
+++ Figlotech.Core/FiAsyncLock.cs
@@ -97,45 +97,10 @@ namespace Figlotech.Core {
 
     public sealed class FiAsyncLock {
         SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
-        public async Task<T> Lock<T>(Func<Task<T>> Try, Func<Exception, Task<T>> Catch = null, Func<bool, Task> Finally = null) {
-            bool isSuccess = true;
-            await _semaphore.WaitAsync();
-            try {
-                return await Try();
-            } catch (Exception x) {
-                isSuccess = false;
-                if (Catch != null) {
-                    return await Catch.Invoke(x);
-                } else {
-                    throw x;
-                }
-            } finally {
-                try {
-                    if (Finally != null) {
-                        await Finally.Invoke(isSuccess);
-                    }
-                } catch (Exception fiex) {
-                    Fi.Tech.Throw(fiex);
-                }
-                _semaphore.Release();
-            }
-        }
 
         public async Task<FiAsyncDisposableLock> Lock() {
             await _semaphore.WaitAsync();
             return new FiAsyncDisposableLock(_semaphore);
-        }
-
-        public Task Lock(Func<Task> Try, Func<Exception, Task> Catch = null, Func<bool, Task> Finally = null) {
-            return Lock<int>(
-                async () => {
-                    await Try();
-                    return 0;
-                }, Catch != null ? async (x) => {
-                    await Catch?.Invoke(x);
-                    return 0;
-                } : (Func<Exception, Task<int>>) null, 
-                Finally);
         }
     }
 }
