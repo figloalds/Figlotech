3d5bcc5dfcb92824d17d21ca20f459c2b68baa66
Felype Rennan <felyperennan@hotmail.com>
2026-02-11 09:05:10 -0300

    ✨feat: Enhance query parameter handling and add IEnumerable chunking
    IQueryBuilder.cs:
    - Introduced a `IsSupportedParameterType` method to explicitly define which types can be used as direct query parameters.
    - Modified the query parameter assignment logic to use this new method.
    - For types not explicitly supported, the parameter value will now be converted to a string representation before being assigned, preventing potential errors with unsupported object types in queries.
    
    IEnumerableExtensions.cs:
    - Added a new `Chunk` extension method to `IEnumerable<T>`.
    - This method allows easily dividing a collection into smaller sub-collections (chunks) of a specified maximum size.
    - Optimized implementations are provided for both arrays and general enumerables for better performance.

--- Figlotech.Core/Data/IQueryBuilder.cs
+++ Figlotech.Core/Data/IQueryBuilder.cs
@@ -1,15 +1,66 @@
-﻿using Figlotech.Core;
+using Figlotech.Core;
 using Figlotech.Core.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.IO;
+using System.Numerics;
 using System.Text;
 
 namespace Figlotech.Data
 {
 
     public static class IQueryBuilderExtensions {
+
+        private static bool IsSupportedParameterType(Type type) {
+            if (type == null) {
+                return false;
+            }
+
+            if (type.IsEnum || typeof(Stream).IsAssignableFrom(type)) {
+                return true;
+            }
+
+            if (type == typeof(bool)
+                || type == typeof(byte)
+                || type == typeof(char)
+                || type == typeof(double)
+                || type == typeof(float)
+                || type == typeof(int)
+                || type == typeof(long)
+                || type == typeof(sbyte)
+                || type == typeof(short)
+                || type == typeof(uint)
+                || type == typeof(ulong)
+                || type == typeof(ushort)
+                || type == typeof(BigInteger)
+                || type == typeof(DateTime)
+                || type == typeof(DateTimeOffset)
+                || type == typeof(decimal)
+                || type == typeof(Guid)
+                || type == typeof(string)
+                || type == typeof(TimeSpan)
+                || type == typeof(byte[])
+                || type == typeof(float[])
+                || type == typeof(StringBuilder)
+                || type == typeof(DBNull)
+                || type.FullName == "System.DateOnly"
+                || type.FullName == "System.TimeOnly") {
+                return true;
+            }
+
+            if (type.IsGenericType) {
+                var genericType = type.GetGenericTypeDefinition();
+                var genericTypeArg = type.GetGenericArguments()[0];
+                if ((genericType == typeof(ArraySegment<>) || genericType == typeof(Memory<>) || genericType == typeof(ReadOnlyMemory<>))
+                    && (genericTypeArg == typeof(byte) || genericTypeArg == typeof(float) || genericTypeArg == typeof(char))) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         public static IQueryBuilder ToQueryBuilder(this string query, params object[] args) {
             return Qb.Fmt(query, args);
@@ -40,8 +91,11 @@ namespace Figlotech.Data
                     cmdParam.DbType = DbType.String;
                 } else if (usableValue.GetType().IsEnum) {
                     cmdParam.Value = (int)usableValue;
-                } else {
+                } else if (IsSupportedParameterType(usableValue.GetType())) {
                     cmdParam.Value = usableValue;
+                } else {
+                    cmdParam.Value = usableValue.ToString() ?? String.Empty;
+                    cmdParam.DbType = DbType.String;
                 }
                 cmdParam.Direction = ParameterDirection.Input;
 
