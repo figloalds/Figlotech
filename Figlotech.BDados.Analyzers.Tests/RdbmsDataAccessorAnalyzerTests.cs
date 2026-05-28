1b881673bc1c1eaef63541f813963d6bb3e49681
Felype Rennan <felyperennan@hotmail.com>
2026-05-28 14:48:07 -0300

    chore: add analyzer test project

---
+++ Figlotech.BDados.Analyzers.Tests/RdbmsDataAccessorAnalyzerTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Figlotech.BDados.Analyzers.Tests {
+    public class RdbmsDataAccessorAnalyzerTests {
+        // Tests will be added in subsequent tasks
+    }
+}
